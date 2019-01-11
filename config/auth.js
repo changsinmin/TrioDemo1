d715d08c8d95d3cde4acff61e44813d6c0274421
Milton Chang <milton@domanisystems.com>
2019-01-11 13:34:41 -0500

    merge auth.js and database.js

--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,13 @@
+// expose our config directly to our application using module.exports
+// milton public key 04591efbb516c6e4dd78f5cb1baf11139b9f5112b9d79b6bbc6b2b6fec9f275680e1388487ac10219618513a8
 module.exports = {
-
-    'trioIkey': '',
-
-    'trioSkey': '',
-
-    
-
+    'trioIkey': '8d9yYNFmveeQtl1hTRzAd36dHKOZwHo8',
+    'trioSkey': 'VrWZwjFCiAnWAMqkwKwlfcQckhNdCkZM',
+    'googleClientId': '969572060105-s7gqmjfvl3albra2ne2ceup4bkren6pg.apps.googleusercontent.com',
+    'googleClientSecrete': '8pHh-_keUnoUDxoBbLBZecpu',
+    'twitterAuth': {
+        'consumerKey': 'kW2UgzdZZg4ZhVSUvv3qOR4CH',
+        'consumerSecret': 'jqv3lwrExcVigLQQXy00KqTlze1FI3mEQbrZIGhv8nbX08YcRR',
+        'callbackURL': 'http://localhost:5000/auth/twitter/callback'
+    }
 };
--- config/database.js
+++ config/database.js
@@ -1,5 +1,3 @@
 module.exports = {
-
-    'url': 'mongodb://admin:xxxxxxxxxx@yyyyyyyyyyy.mlab.com:47233/demo1-dev'
-
+    'url': 'mongodb://admin:domani2011@ds147233.mlab.com:47233/demo1-dev'
 };
